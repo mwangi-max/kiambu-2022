8eba12f8a49281bddb2a0b975e23fceb6e5c98c9
Kelvin Kamau <kelvinkam24@gmail.com>
2022-09-16 15:46:05 +0300

    modified css

--- kiambu-leaders.js
+++ kiambu-leaders.js
@@ -25,6 +25,18 @@ closeAsideBtn.addEventListener('click', ()=>{
     }
 })
 
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
+
+
 
 
 
